099319b606f9f8cef91af78d58bef478e7970eed
tigertouristua-glitch <tiger.tourist.ua@gmail.com>
2025-09-20 13:50:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
+const CACHE = "guru-coffee-v4";
+const ASSETS = ["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install", e=>{
-  e.waitUntil(caches.open("gc-v1").then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"])));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate", e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
 });
 self.addEventListener("fetch", e=>{
   e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
